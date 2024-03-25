32176d3e96b0126f7a3e0e9a9f24039589c42cd6
Abhinav <abhinavkumar34002@gmail.com>
2024-03-26 03:40:11 +0530

    Added images and icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
     return (
-        <div className="h-screen">
+        <div className="h-screen px-8 py-2">
             <UserButton />
             Namaste
         </div>
